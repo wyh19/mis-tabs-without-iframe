9d876cb51e13ffed085858e6891b6c7042e33721
wyh19 <wyh_19@163.com>
2019-03-18 18:06:49 +0800

    修改配置

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -166,7 +166,7 @@ module.exports = {
                                         before: [tsImportPluginFactory({
                                             libraryName: 'antd',
                                             libraryDirectory: 'lib',
-                                            style: true
+                                            style: 'css'
                                         })]
                                     }),
                                     compilerOptions: {
@@ -181,6 +181,39 @@ module.exports = {
                     // "style" loader turns CSS into JS modules that inject <style> tags.
                     // In production, we use a plugin to extract that CSS to a file, but
                     // in development "style" loader enables hot editing of CSS.
+                    {
+                        test: /\.css$/,
+                        use: [
+                            require.resolve('style-loader'),
+                            {
+                                loader: require.resolve('css-loader'),
+                                options: {
+                                    importLoaders: 1,
+                                },
+                            },
+                            {
+                                loader: require.resolve('postcss-loader'),
+                                options: {
+                                    // Necessary for external CSS imports to work
+                                    // https://github.com/facebookincubator/create-react-app/issues/2677
+                                    ident: 'postcss',
+                                    plugins: () => [
+                                        require('postcss-flexbugs-fixes'),
+                                        autoprefixer({
+                                            browsers: [
+                                                '>1%',
+                                                'last 4 versions',
+                                                'Firefox ESR',
+                                                'not ie < 9', // React doesn't support IE8 anyway
+                                            ],
+                                            flexbox: 'no-2009',
+                                        }),
+                                    ],
+                                    sourceMap: true,
+                                },
+                            },
+                        ]
+                    },
                     {
                         //test: /\.css$/,
                         test: /\.s?css$/,
